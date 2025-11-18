64d4582dd2b50426ad6e794133d303824e1e3934
sriman reddy <2203a54046@sru.edu.in>
2025-11-19 00:08:29 +0530

    Delete backend directory

--- backend/models/model testing.py
+++
@@ -1,5 +0,0 @@
-import onnxruntime
-
-model_path = "C:/Users/Sriman Reddy/Downloads/MissingPersonDetect/MissingPersonDetect/backend/models/reid_model.onnx"
-session = onnxruntime.InferenceSession(model_path)
-print("Model loaded successfully!")
